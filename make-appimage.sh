2eab411909427f175d90927056e97b3af9eaa7cb
Link4Electronics <link4electronics@gmail.com>
2026-02-03 00:57:25 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,19 +3,19 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q isle-portable-git | awk '{print $2; exit}') # example command to get version of application here
+#VERSION=$(pacman -Q isle-portable-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/$HOME/.local/share/icons/hicolor/scalable/apps/org.legoisland.Isle.svg
-export DESKTOP=/$HOME/.local/share/applications/org.legoisland.Isle.desktop
+export ICON=/usr/share/icons/hicolor/scalable/apps/org.legoisland.Isle.svg
+export DESKTOP=/usr/share/applications/org.legoisland.Isle.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /$HOME/.local/bin/isle /usr/bin/isle-config
-mv $HOME/.local/lib/liblego1.so ./AppDir/bin
+quick-sharun /usr/bin/isle /usr/bin/isle-config
+mv usr/lib/liblego1.so ./AppDir/bin
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
